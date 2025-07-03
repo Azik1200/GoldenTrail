4ac5fa38deec193bcb9732916e8ce172343716eb
Aziz Salimli <62033056+Azik1200@users.noreply.github.com>
2025-07-03 10:17:23 +0400

    Map full product description

--- src/utils/transformProduct.js
+++ src/utils/transformProduct.js
@@ -22,6 +22,10 @@ export default function transformProduct(product) {
     is_new: product.is_new,
     is_popular: product.is_popular,
     is_on_sale: product.is_on_sale,
-    desc: product.description || product.desc || '',
+    desc:
+      product.full_description ||
+      product.description ||
+      product.desc ||
+      '',
   };
 }
